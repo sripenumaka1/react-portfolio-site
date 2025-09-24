676f5ae2931adbc13aba7c00c7d4155c436c341f
sripenumaka1 <sridattapenumaka@gmail.com>
2025-09-23 19:56:38 -0700

    Update ProjectDetails1.jsx

--- src/pages/ProjectDetails1.jsx
+++ src/pages/ProjectDetails1.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ProjectDetails from './ProjectDetails';
 import gokuInSpaceImg from '../assets/images/gokuinspace.png';
 
-const description = `This creative coding piece features Goku peacefully floating through outer space, brought to life with CSS keyframe animations and animated GIFs. The scene is designed to feel dynamic and immersive while remaining lightweight and performant.`;
+const description = `This creative coding piece features Son Goku peacefully floating through outer space, brought to life with CSS keyframe animations and animated GIFs. The scene is designed to feel dynamic and immersive while remaining lightweight and performant.`;
 
 export default function ProjectDetails1() {
   return (
